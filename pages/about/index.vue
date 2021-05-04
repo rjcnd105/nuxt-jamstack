01d21594f02b47f1b76297c7c606f6b133a03cde
hj_macbook <rjcnd123@gmail.com>
2021-05-04 10:03:50 +0900

    build config(postcss)

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,28 +1,6 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">nuxt-jamstack</h1>
-      <NuxtLink to="/about"></NuxtLink>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+     im about
   </div>
 </template>
 
