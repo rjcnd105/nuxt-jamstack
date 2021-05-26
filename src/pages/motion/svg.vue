30eddc1f2c5206d3a8896b097b683e606be2b008
hj_macbook <rjcnd123@gmail.com>
2021-05-26 22:19:43 +0900

    svg 공부중

--- src/pages/motion/svg.vue
+++ src/pages/motion/svg.vue
@@ -1,6 +1,39 @@
 <template>
   <div :class="$style.page" class="bg-gray-0">
-    <svg version="1.1"></svg>
+    <!--    viewBox: x, y, width, height-->
+    w100 h100, vb 0 0 100 100
+    <svg width="100" height="100" viewBox="0 0 100 100">
+      <circle
+        r="10"
+        cx="25"
+        cy="25"
+        stroke="#f00"
+        fill="none"
+        stroke-width="3"
+      ></circle>
+    </svg>
+    w100 h100, vb 0 0 50 50
+    <svg width="100" height="100" viewBox="0 0 50 50">
+      <circle
+        r="10"
+        cx="25"
+        cy="25"
+        stroke="#f00"
+        fill="none"
+        stroke-width="3"
+      ></circle>
+    </svg>
+    둘 다 같은 r10 cx25 cy25인 원임. viewbox 25 25 50 50
+    <svg width="100" height="100" viewBox="25 25 50 50">
+      <circle
+        r="10"
+        cx="25"
+        cy="25"
+        stroke="#f00"
+        fill="none"
+        stroke-width="3"
+      ></circle>
+    </svg>
   </div>
 </template>
 
@@ -18,7 +51,6 @@ export default {
 <style lang="postcss" module scoped>
 .page {
   height: 20000px;
-  padding-top: 1000px;
   @apply mx-auto w-full xl:max-w-1200 px-10;
 }
 </style>
